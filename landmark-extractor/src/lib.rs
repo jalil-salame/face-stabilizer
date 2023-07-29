ad24758fbd7666b64d89f20e99c25df5092a6811
Jalil David Salamé Messina <jalil.salame@gmail.com>
2023-07-29 13:36:12 +0200

    feat: Split into library
    Also removed buggy projection code

---
+++ landmark-extractor/src/lib.rs
@@ -0,0 +1,139 @@
+use dlib_face_recognition::FaceDetectorTrait;
+use dlib_face_recognition::FaceLandmarks;
+use dlib_face_recognition::ImageMatrix;
+use dlib_face_recognition::LandmarkPredictorTrait;
+use dlib_face_recognition::Point;
+use dlib_face_recognition::Rectangle;
+
+/// Any number of [`Face`]s
+#[derive(Debug, Clone)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub struct Faces(Box<[Face]>);
+
+impl std::ops::Deref for Faces {
+    type Target = [Face];
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl From<Faces> for Box<[Face]> {
+    fn from(value: Faces) -> Self {
+        value.0
+    }
+}
+
+/// The bounding box ([`Rect`]) and [`Landmarks`] of a face
+#[derive(Debug, Clone)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub struct Face(pub Rect, pub Landmarks);
+
+impl From<Face> for (Rectangle, Landmarks) {
+    fn from(value: Face) -> Self {
+        (value.0.into(), value.1)
+    }
+}
+
+/// A bounding box
+///
+/// Derives serde traits, unlike the [`dlib_face_recognition::Rectangle`]
+#[derive(Debug, Clone)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub struct Rect {
+    pub left: i64,
+    pub top: i64,
+    pub right: i64,
+    pub bottom: i64,
+}
+
+impl From<Rect> for Rectangle {
+    fn from(value: Rect) -> Self {
+        let Rect {
+            left,
+            top,
+            right,
+            bottom,
+        } = value;
+
+        Self {
+            left,
+            top,
+            right,
+            bottom,
+        }
+    }
+}
+
+impl From<Rectangle> for Rect{
+    fn from(value: Rectangle) -> Self {
+        let Rectangle {
+            left,
+            top,
+            right,
+            bottom,
+        } = value;
+
+        Self {
+            left,
+            top,
+            right,
+            bottom,
+        }
+    }
+}
+
+/// Facial Landmarks
+///
+/// Derives more traits unlike [`dlib_face_recognition::FaceLandmarks`]
+#[derive(Debug, Clone)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub struct Landmarks(Box<[(i64, i64)]>);
+
+impl std::ops::Deref for Landmarks {
+    type Target = [(i64, i64)];
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl From<Landmarks> for Box<[Point]> {
+    fn from(value: Landmarks) -> Self {
+        value
+            .0
+            .into_vec()
+            .into_iter()
+            .map(|(x, y)| Point::new(x, y))
+            .collect()
+    }
+}
+
+impl From<FaceLandmarks> for Landmarks {
+    fn from(value: FaceLandmarks) -> Self {
+        Self(value.to_vec().into_iter().map(|p| (p.x(), p.y())).collect())
+    }
+}
+
+/// Find all faces in this image and identify the landmarks in it
+pub fn extract_landmarks(
+    image: &ImageMatrix,
+    detector: &impl FaceDetectorTrait,
+    predictor: &impl LandmarkPredictorTrait,
+) -> Faces {
+    let landmarks = detector
+        .face_locations(image)
+        .iter()
+        .cloned()
+        .map(|face| Face(face.into(), predictor.face_landmarks(image, &face).into()))
+        .collect();
+
+    Faces(landmarks)
+}
+
+/// Helper function to load an [`ImageMatrix`] from a path
+#[cfg(feature = "image")]
+pub fn img_mat_from_path(img_path: &std::path::Path) -> image::ImageResult<ImageMatrix> {
+    let image = image::open(&img_path)?.into_rgb8();
+    Ok(ImageMatrix::from_image(&image))
+}
--- src/main.rs
+++ src/main.rs
@@ -2,182 +2,112 @@ use anyhow::anyhow;
 use anyhow::bail;
 use anyhow::Context;
 use clap::Parser;
+use clap::Subcommand;
 use dlib_face_recognition::FaceDetector;
-use dlib_face_recognition::FaceDetectorTrait;
-use dlib_face_recognition::FaceLandmarks;
 use dlib_face_recognition::ImageMatrix;
 use dlib_face_recognition::LandmarkPredictor;
-use dlib_face_recognition::LandmarkPredictorTrait;
-use dlib_face_recognition::Rectangle;
-use imageproc::geometric_transformations::warp;
-use imageproc::geometric_transformations::Interpolation;
-use imageproc::geometric_transformations::Projection;
 use log::debug;
-use log::error;
 use log::info;
 use log::warn;
 
 #[derive(Debug, Parser)]
 struct Opts {
-    /// Path to the Shape Predictor model (also called Facial Landmarks Predictor)
-    #[arg(env, short, long)]
-    shape_predictor: std::path::PathBuf,
-    /// Images of faces to stabilize
-    #[arg(num_args(1..))]
-    images: Vec<std::path::PathBuf>,
-    /// Directory to place the stabilized images in
-    #[arg(long, short, default_value = "./out")]
-    out_dir: std::path::PathBuf,
-    /// Reference image (if none is provided the first of the images will be used)
-    #[arg(short, long)]
-    reference: Option<std::path::PathBuf>,
+    #[command(subcommand)]
+    command: Actions,
+}
+
+#[derive(Debug, Subcommand)]
+enum Actions {
+    /// Extract Features from images to process later
+    ExtractFeatures {
+        /// Path to the Shape Predictor model (also called Facial Landmarks Predictor)
+        #[arg(env, short, long)]
+        shape_predictor: std::path::PathBuf,
+        /// Path to a directory containing the images you want to extract the features of
+        image_dir: std::path::PathBuf,
+        /// Path to the output file
+        #[arg(short, long, default_value = "landmarks.ron")]
+        output: std::path::PathBuf,
+    },
 }
 
 fn main() -> anyhow::Result<()> {
     // Configure using RUST_LOG=* (ie. RUST_LOG=info)
     env_logger::init();
 
-    let mut opts = Opts::parse();
+    let opts = Opts::parse();
     debug!("Recieved {opts:?}");
 
-    if !opts.out_dir.exists() {
-        info!(
-            "{dir} does not exist, creating it",
-            dir = opts.out_dir.display()
-        );
-        std::fs::create_dir(&opts.out_dir)
-            .with_context(|| format!("Failed to create {}", opts.out_dir.display()))?;
-    }
-
-    let reference = opts.reference.unwrap_or_else(|| opts.images.swap_remove(0));
-
-    if !opts.shape_predictor.is_file() {
-        bail!(
-            "{file} is not a regular file.",
-            file = opts.shape_predictor.display()
-        );
-    }
-
-    let detector = FaceDetector::new();
-
-    info!(
-        "Loading shape predictor from {file}",
-        file = opts.shape_predictor.display()
-    );
-    let predictor = LandmarkPredictor::open(opts.shape_predictor).map_err(|err| anyhow!(err))?;
-
-    // Extract landmarks for reference image
-    let reference_landmarks =
-        extract_landmarks(&img_mat_from_path(&reference)?, &detector, &predictor)
-            .with_context(|| format!("failed to get landmarks for reference image"))?;
-
-    let out_path = |path: &std::path::Path| opts.out_dir.join(path.file_name().unwrap());
-    let copy_to_out_dir = |path: &std::path::Path| std::fs::copy(path, out_path(path));
-
-    // Save reference to out dir
-    copy_to_out_dir(&reference).context("failed to copy file to output directory")?;
-
-    // Get image paths
-    let img_paths = {
-        let mut imgs = opts.images;
-        imgs.sort_unstable();
-        imgs
-    };
-
-    // Extract landmarks
-    for img_path in img_paths {
-        match stabilize(&img_path, &reference_landmarks, &detector, &predictor) {
-            Err(err) => warn!("{err}"),
-            Ok(image) => {
-                if let Err(err) = image
-                    .save(out_path(&img_path))
-                    .context("failed to save image")
-                {
-                    error!("{err}");
-                }
+    match opts.command {
+        Actions::ExtractFeatures {
+            shape_predictor,
+            image_dir,
+            output,
+        } => {
+            if output.exists() {
+                warn!("{} exists, making a backup", output.display());
+                let mut backup = output.clone();
+                backup.set_extension(output.extension().map_or("bak".to_string(), |ext| {
+                    format!("{}.bak", ext.to_str().unwrap_or(""))
+                }));
+                std::fs::rename(&output, backup).context("trying to backup the ouput file")?;
             }
-        };
+            let output = std::fs::File::create(output)?;
+
+            let detector = FaceDetector::new();
+
+            let file = shape_predictor.display();
+            info!("Loading shape predictor from {file}",);
+            if !shape_predictor.is_file() {
+                bail!("{file} is not a regular file (or doesn't exist).",);
+            }
+            let predictor = LandmarkPredictor::open(shape_predictor).map_err(|err| anyhow!(err))?;
+
+            let image_paths: Vec<_> = std::fs::read_dir(image_dir)
+                .context("trying to open image_dir")?
+                .filter_map(|dir_ent| -> Option<anyhow::Result<std::path::PathBuf>> {
+                    let Ok(dir_ent) = dir_ent else { return Some(Err(dir_ent.unwrap_err().into())) };
+                    let ft = match dir_ent
+                        .file_type()
+                        .with_context(|| format!("trying to get the file type of {}", dir_ent.file_name().to_string_lossy()))
+                    {
+                        Ok(ft) => ft,
+                        Err(err) => return Some(Err(err)),
+                    };
+                    if !ft.is_file() {
+                        info!("{} is not a file, skipping", dir_ent.file_name().to_string_lossy());
+                        return None;
+                    }
+                    Some(Ok(dir_ent.path()))
+                }).collect::<anyhow::Result<_>>()?;
+
+            use indicatif::ProgressIterator;
+            let style = indicatif::ProgressStyle::with_template(
+                "[{pos:>4}/{len:4}] {msg} {bar} [{per_sec} {eta_precise}]",
+            )
+            .expect("valid template");
+            let features: std::collections::HashMap<_, _> = image_paths
+                .into_iter()
+                .progress_with_style(style)
+                .map(
+                    |path| -> anyhow::Result<(std::path::PathBuf, landmark_extractor::Faces)> {
+                        let img = image::open(&path)
+                            .with_context(|| format!("failed to open {}", path.display()))?
+                            .into_rgb8();
+                        let mat = ImageMatrix::from_image(&img);
+                        let landmarks =
+                            landmark_extractor::extract_landmarks(&mat, &detector, &predictor);
+                        Ok((path, landmarks))
+                    },
+                )
+                .collect::<anyhow::Result<_>>()?;
+
+            info!("finished processing");
+            info!("serializing to file");
+            ron::ser::to_writer_pretty(output, &features, ron::ser::PrettyConfig::default())
+                .context("serializing landmarks to a file")?;
+        }
     }
 
     Ok(())
-}
-
-fn stabilize(
-    img_path: &std::path::Path,
-    reference: &FaceLandmarks,
-    detector: &FaceDetector,
-    predictor: &LandmarkPredictor,
-) -> anyhow::Result<image::RgbImage> {
-    let image = image::open(&img_path)?.into_rgb8();
-    let img_mat = ImageMatrix::from_image(&image);
-
-    let landmarks = extract_landmarks(&img_mat, detector, predictor)
-        .with_context(|| format!("while processing {}", img_path.display()))?;
-
-    let from = points(reference);
-    let to = points(&landmarks);
-
-    for (from, to) in choose(from).into_iter().zip(choose(to)) {
-        let Some(proj) = Projection::from_control_points(from, to) else { continue; };
-        return Ok(warp(
-            &image,
-            &proj,
-            Interpolation::Bicubic,
-            image::Rgb([0, 0, 0]),
-        ));
-    }
-
-    bail!(
-        "failed to find a valid projection for {}",
-        img_path.display()
-    )
-}
-
-/// Returns [nose tip, left eye, left eye, right eye, right eye]
-fn points(landmarks: &FaceLandmarks) -> [(f32, f32); 5] {
-    [
-        (landmarks[34 - 1].x() as f32, landmarks[34 - 1].y() as f32),
-        (landmarks[37 - 1].x() as f32, landmarks[37 - 1].y() as f32),
-        (landmarks[40 - 1].x() as f32, landmarks[40 - 1].y() as f32),
-        (landmarks[43 - 1].x() as f32, landmarks[43 - 1].y() as f32),
-        (landmarks[46 - 1].x() as f32, landmarks[46 - 1].y() as f32),
-    ]
-}
-
-fn choose(points: [(f32, f32); 5]) -> [[(f32, f32); 4]; 4] {
-    [
-        // [points[1], points[2], points[3], points[4]],
-        [points[0], points[2], points[3], points[4]],
-        [points[0], points[1], points[3], points[4]],
-        [points[0], points[1], points[2], points[4]],
-        [points[0], points[1], points[2], points[3]],
-    ]
-}
-
-fn img_mat_from_path(img_path: &std::path::Path) -> image::ImageResult<ImageMatrix> {
-    let image = image::open(&img_path)?.into_rgb8();
-    Ok(ImageMatrix::from_image(&image))
-}
-
-// TODO: Switch to custom result so I can handle the cases with multiple faces
-fn extract_landmarks(
-    image: &ImageMatrix,
-    detector: &FaceDetector,
-    predictor: &LandmarkPredictor,
-) -> anyhow::Result<FaceLandmarks> {
-    let faces = detector.face_locations(&image);
-    let faces: &[Rectangle] = &faces;
-
-    let face = match faces {
-        [] => {
-            bail!("No face found");
-        }
-        [face] => face,
-        _ => {
-            bail!("Multiple faces found");
-        }
-    };
-
-    info!("Found face {face:?}");
-    Ok(predictor.face_landmarks(&image, &face))
 }
