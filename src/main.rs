20c6a5e9eebbe2d09a392a890ff3c0ed38f917b4
Jalil David Salamé Messina <jalil.salame@gmail.com>
2024-07-12 19:30:09 +0200

    feat: switch from anyhow to miette

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,6 @@ use std::collections::HashMap;
 use std::path::Path;
 use std::path::PathBuf;
 
-use anyhow::anyhow;
-use anyhow::bail;
-use anyhow::ensure;
-use anyhow::Context;
 use clap::Parser;
 use clap::Subcommand;
 use dlib_face_recognition::FaceDetector;
@@ -18,14 +14,53 @@ use landmark_extractor::Landmarks;
 use log::debug;
 use log::info;
 use log::warn;
+use miette::bail;
+use miette::ensure;
+use miette::Context;
+use miette::IntoDiagnostic;
+use miette::Result;
 
 #[cfg(feature = "gui")]
 mod gui;
+
+fn main() -> Result<()> {
+    // Pretty panics
+    miette::set_panic_hook();
+    // Configure using RUST_LOG=* (ie. RUST_LOG=info)
+    env_logger::init();
+    // Parse opts
+    let opts = Opts::parse();
+    debug!("Received {opts:?}");
+    opts.run()
+}
 
 #[derive(Debug, Parser)]
 struct Opts {
     #[command(subcommand)]
     command: Actions,
+}
+
+impl Opts {
+    fn run(self) -> Result<()> {
+        match self.command {
+            Actions::ExtractFeatures {
+                shape_predictor,
+                image_dir,
+                output,
+                pretty,
+            } => extract_features(&shape_predictor, &image_dir, &output, pretty),
+            Actions::Transform {
+                features,
+                output_dir,
+            } => transform_images(&features, &output_dir),
+            #[cfg(feature = "gui")]
+            Actions::GUI => gui::Gui::run(iced::Settings {
+                // default_font: iced::Font::with_name("DejaVu Sans"),
+                ..iced::Settings::default()
+            })
+            .context("running gui"),
+        }
+    }
 }
 
 #[derive(Debug, Subcommand)]
@@ -56,127 +91,122 @@ enum Actions {
     GUI,
 }
 
-fn main() -> anyhow::Result<()> {
-    // Configure using RUST_LOG=* (ie. RUST_LOG=info)
-    env_logger::init();
+fn transform_images(features: &Path, output_dir: &Path) -> Result<()> {
+    ensure!(features.exists(), "could not find {}", features.display());
+    ensure!(features.is_file(), "{} is not a file", features.display());
+    let file = std::fs::File::open(features)
+        .into_diagnostic()
+        .context("opening features file")?;
+    let features: Features = ron::de::from_reader(file)
+        .into_diagnostic()
+        .context("deserializing features")?;
+    let mut features: Vec<_> = features.into_iter().collect();
+    features.sort_by_cached_key(|f| f.0.clone());
+    if !output_dir.exists() {
+        std::fs::create_dir(output_dir)
+            .into_diagnostic()
+            .with_context(|| format!("creating {} directory", output_dir.display()))?;
+    } else {
+        ensure!(
+            output_dir.is_dir(),
+            "{} is not a directory",
+            output_dir.display()
+        );
+    }
+    let out_path = |file: &Path| output_dir.join(file.file_name().expect("valid file name"));
 
-    let opts = Opts::parse();
-    debug!("Received {opts:?}");
-
-    match opts.command {
-        Actions::ExtractFeatures {
-            shape_predictor,
-            image_dir,
-            output,
-            pretty,
-        } => extract_features(shape_predictor, image_dir, output, pretty),
-        Actions::Transform {
-            features,
-            output_dir,
-        } => {
-            ensure!(features.exists(), "could not find {}", features.display());
-            ensure!(features.is_file(), "{} is not a file", features.display());
-            let file = std::fs::File::open(features).context("opening features file")?;
-            let features: Features =
-                ron::de::from_reader(file).context("deserializing features")?;
-            let mut features: Vec<_> = features.into_iter().collect();
-            features.sort_by_cached_key(|f| f.0.clone());
-            if !output_dir.exists() {
-                std::fs::create_dir(&output_dir)
-                    .with_context(|| format!("creating {} directory", output_dir.display()))?;
-            } else {
-                ensure!(
-                    output_dir.is_dir(),
-                    "{} is not a directory",
-                    output_dir.display()
-                );
-            }
-            let out_path =
-                |file: &Path| output_dir.join(file.file_name().expect("valid file name"));
-
-            let (ref_path, ref_feat) = features.swap_remove(0);
-            ensure!(
-                ref_feat.len() == 1,
-                "reference face should have exactly one face"
-            );
-            let (_, ref_feat) = ref_feat.iter().next().unwrap().clone().into();
-            std::fs::copy(&ref_path, out_path(&ref_path))?;
-
-            use indicatif::*;
-            let style = ProgressStyle::with_template(
-                "[{pos:>4}/{len:4}] {msg} {bar} [{per_sec} {eta_precise}]",
+    let (ref_path, ref_feat) = features.swap_remove(0);
+    ensure!(
+        ref_feat.len() == 1,
+        "reference face should have exactly one face"
+    );
+    let (_, ref_feat) = ref_feat.iter().next().unwrap().clone().into();
+    std::fs::copy(&ref_path, out_path(&ref_path))
+        .into_diagnostic()
+        .with_context(|| {
+            format!(
+                "failed to copy {} to {}",
+                ref_path.display(),
+                out_path(&ref_path).display()
             )
+        })?;
+
+    use indicatif::*;
+    let style =
+        ProgressStyle::with_template("[{pos:>4}/{len:4}] {msg} {bar} [{per_sec} {eta_precise}]")
             .expect("valid template");
 
-            #[cfg(feature = "rayon")]
-            use rayon::prelude::*;
-            #[cfg(feature = "rayon")]
-            let features = features.into_par_iter();
-            #[cfg(not(feature = "rayon"))]
-            let features = features.into_iter();
+    #[cfg(feature = "rayon")]
+    use rayon::prelude::*;
+    #[cfg(feature = "rayon")]
+    let features = features.into_par_iter();
+    #[cfg(not(feature = "rayon"))]
+    let features = features.into_iter();
 
-            features
-                .progress_with_style(style)
-                .map(|(img_path, img_feat)| {
-                    if img_feat.len() != 1 {
-                        warn!(
-                            "{} does not have a single face, it has {} instead",
-                            img_path.display(),
-                            img_feat.len()
-                        );
-                        return Ok(());
-                    }
+    features
+        .progress_with_style(style)
+        .map(|(img_path, img_feat)| {
+            if img_feat.len() != 1 {
+                warn!(
+                    "{} does not have a single face, it has {} instead",
+                    img_path.display(),
+                    img_feat.len()
+                );
+                return Ok(());
+            }
 
-                    let (_, img_feat) = img_feat.iter().next().unwrap().clone().into();
-                    let img = image::open(&img_path)
-                        .with_context(|| format!("opening image {}", img_path.display()))?
-                        .into_rgb8();
+            let (_, img_feat) = img_feat.iter().next().unwrap().clone().into();
+            let img = image::open(&img_path)
+                .into_diagnostic()
+                .with_context(|| format!("opening image {}", img_path.display()))?
+                .into_rgb8();
 
-                    let out = out_path(&img_path);
+            let out = out_path(&img_path);
 
-                    apply_projection(&ref_feat, &img_feat, &img)
-                        .save(&out)
-                        .with_context(|| format!("saving image to {}", out.display()))
-                })
-                .collect()
-        }
-        #[cfg(feature = "gui")]
-        Actions::GUI => gui::Gui::run(iced::Settings {
-            // default_font: iced::Font::with_name("DejaVu Sans"),
-            ..iced::Settings::default()
+            apply_projection(&ref_feat, &img_feat, &img, [0, 0, 0].into())
+                .save(&out)
+                .into_diagnostic()
+                .with_context(|| format!("saving image to {}", out.display()))
         })
-        .context("running gui"),
-    }
+        .collect()
 }
 
 fn extract_features(
-    shape_predictor: PathBuf,
-    image_dir: PathBuf,
-    output: PathBuf,
+    shape_predictor: &Path,
+    image_dir: &Path,
+    output: &Path,
     pretty: bool,
-) -> anyhow::Result<()> {
+) -> Result<()> {
     if output.exists() {
         warn!("{} exists, making a backup", output.display());
-        let mut backup = output.clone();
+        let mut backup = output.to_path_buf();
         backup.set_extension(output.extension().map_or("bak".to_string(), |ext| {
             format!("{}.bak", ext.to_str().unwrap_or(""))
         }));
-        std::fs::rename(&output, backup).context("trying to backup the output file")?;
+        std::fs::rename(output, backup)
+            .into_diagnostic()
+            .context("trying to backup the output file")?;
     }
-    let output = std::fs::File::create(output)?;
+    let output = std::fs::File::create(output).into_diagnostic()?;
 
     let file = shape_predictor.display();
     info!("Loading shape predictor from {file}",);
     if !shape_predictor.is_file() {
         bail!("{file} is not a regular file (or doesn't exist).",);
     }
-    let predictor = LandmarkPredictor::open(shape_predictor).map_err(|err| anyhow!(err))?;
+    let predictor = LandmarkPredictor::open(shape_predictor).map_err(|err| miette::miette!(err))?;
 
     let image_paths: Vec<_> = std::fs::read_dir(image_dir)
+        .into_diagnostic()
         .context("trying to open image_dir")?
-        .filter_map(|dir_ent| -> Option<anyhow::Result<PathBuf>> {
-            let Ok(dir_ent) = dir_ent else { return Some(Err(dir_ent.unwrap_err().into())) };
-            let ft = match dir_ent.file_type().with_context(|| {
+        .filter_map(|dir_ent| -> Option<Result<PathBuf>> {
+            let dir_ent = match dir_ent {
+                Ok(dir_ent) => dir_ent,
+                Err(err) => {
+                    return Some(Err(err).into_diagnostic());
+                }
+            };
+            let ft = match dir_ent.file_type().into_diagnostic().with_context(|| {
                 format!(
                     "trying to get the file type of {}",
                     dir_ent.file_name().to_string_lossy()
@@ -194,7 +224,7 @@ fn extract_features(
             }
             Some(Ok(dir_ent.path()))
         })
-        .collect::<anyhow::Result<_>>()?;
+        .collect::<Result<_>>()?;
 
     use indicatif::*;
     let style =
@@ -211,8 +241,9 @@ fn extract_features(
 
     let features: Features = iter
         .progress_with_style(style)
-        .map(|path| -> anyhow::Result<(PathBuf, Faces)> {
+        .map(|path| -> Result<(PathBuf, Faces)> {
             let img = image::open(&path)
+                .into_diagnostic()
                 .with_context(|| format!("failed to open {}", path.display()))?
                 .into_rgb8();
             let mat = ImageMatrix::from_image(&img);
@@ -220,7 +251,7 @@ fn extract_features(
             let landmarks = landmark_extractor::extract_landmarks(&mat, &detector, &predictor);
             Ok((path, landmarks))
         })
-        .collect::<anyhow::Result<_>>()?;
+        .collect::<Result<_>>()?;
 
     info!("finished processing");
     info!("serializing to file");
@@ -229,6 +260,7 @@ fn extract_features(
     } else {
         ron::ser::to_writer(output, &features)
     }
+    .into_diagnostic()
     .context("serializing landmarks to a file")?;
     Ok(())
 }
@@ -239,10 +271,17 @@ fn apply_projection(
     target: &Landmarks,
     points: &Landmarks,
     image: &image::RgbImage,
+    default: image::Rgb<u8>,
 ) -> image::ImageBuffer<image::Rgb<u8>, Vec<u8>> {
-    let target = target.iter().map(|&(x, y)| (x as f32, y as f32).into());
-    let points = points.iter().map(|&(x, y)| (x as f32, y as f32).into());
-    let proj = stabilizer::procrustes_superimposition(target, points)
-        .expect("neither points nor target are empty and they have the same length");
-    warp(image, &proj, Interpolation::Bicubic, image::Rgb([0, 0, 0]))
+    let mut target: Vec<_> = target
+        .iter()
+        .map(|&(x, y)| (x as f32, y as f32).into())
+        .collect();
+    let mut points: Vec<_> = points
+        .iter()
+        .map(|&(x, y)| (x as f32, y as f32).into())
+        .collect();
+    let projection = stabilizer::procrustes_superimposition(&mut target, &mut points)
+        .expect("couldn't project points into target");
+    warp(image, &projection, Interpolation::Bicubic, default)
 }
